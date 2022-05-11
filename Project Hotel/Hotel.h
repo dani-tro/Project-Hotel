8ced7a76d6e11e21f5e125a49c407e59696d2ba7
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-05-12 00:26:29 +0300

    Declared class Hotel

---
+++ Project Hotel/Hotel.h
@@ -0,0 +1,36 @@
+#ifndef __HOTEL_HPP
+#define __HOTEL_HPP
+
+#include "Vector.hpp"
+#include "Room.h"
+
+#include <fstream>
+
+class Hotel
+{
+private:
+	Vector<Room> rooms;
+	std::fstream file;
+public:
+	void open_the_file_with_loaded_rooms();
+	void read_the_available_rooms_from_file();
+	void read_the_loaded_events_in_the_file();
+	void run_hotel();
+	int32_t find_the_index_of_room_with_given_number(uint32_t) const;
+	void process_the_command(String&);
+	void register_a_guest();
+	void print_the_free_rooms();
+	void free_a_room();
+	void print_a_report();
+	void find_a_room();
+	void close_a_room();
+	void print_error_message();
+	int32_t get_the_room_with_the_fewest_beds(uint32_t, Time_Period&);
+	bool is_valid(const Accommodation& );
+	bool is_valid(const Closure&);
+};
+
+void print_available_commands();
+void read_a_command(String&);
+
+#endif
