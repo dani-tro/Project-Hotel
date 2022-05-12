7251fa4b13535dc17b00b8ddea70d026c17e7cd4
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-05-12 20:02:24 +0300

    Added number of guests

--- Project Hotel/Accommodation.cpp
+++ Project Hotel/Accommodation.cpp
@@ -1,8 +1,9 @@
 #include "Accommodation.h"
 
-Accommodation::Accommodation(uint32_t _room_number, const Time_Period& _period, String _guest_name, String _note)
+Accommodation::Accommodation(uint32_t _room_number, uint32_t _number_of_guests, const Time_Period& _period, String _guest_name, String _note)
 {
 	room_number = _room_number;
+	number_of_guests = _number_of_guests;
 	period = _period;
 	guest_name = _guest_name;
 	note = _note;
@@ -33,10 +34,16 @@ const String& Accommodation::get_guest_name() const
 	return guest_name;
 }
 
+uint32_t Accommodation::get_number_of_guests() const
+{
+	return number_of_guests;
+}
+
 void Accommodation::read_accommodation_from_file(std::fstream& file, int position)
 {
 	file.seekg(position);
 	file.read(reinterpret_cast<char*> (&room_number), sizeof(uint32_t));
+	file.read(reinterpret_cast<char*>(number_of_guests), sizeof(uint32_t));
 	period.read_period_from_file(file, file.tellg());
 	guest_name.read_string_from_file(file, file.tellg());
 	note.read_string_from_file(file, file.tellg());
@@ -48,6 +55,7 @@ void Accommodation::write_accommodation_to_file(std::fstream& file, int position
 	file.seekp(position);
 	file.write(reinterpret_cast<const char*> (&accommodation_type), sizeof(uint8_t));
 	file.write(reinterpret_cast<const char*> (&room_number), sizeof(uint32_t));
+	file.write(reinterpret_cast<const char*> (&number_of_guests), sizeof(uint32_t));
 	period.write_period_to_file(file, file.tellp());
 	guest_name.write_string_to_file(file, file.tellp());
 	note.write_string_to_file(file, file.tellp());
@@ -72,6 +80,8 @@ std::istream& operator>>(std::istream& in, Accommodation& accommodation)
 {
 	std::cout << "Enter a room number: ";
 	in >> accommodation.room_number;
+	std::cout << "Enter the number of guests: ";
+	in >> accommodation.number_of_guests;
 	in >> accommodation.period;
 	std::cout << "Enter the guest's name: ";
 	in >> accommodation.guest_name;
--- Project Hotel/Accommodation.h
+++ Project Hotel/Accommodation.h
@@ -4,19 +4,21 @@
 #include "Time_Period.h"
 #include "String.h"
 
-constexpr int32_t position_of_to_date_in_file = 17; // sizeof(uint8_t) + sizeof(uint32_t) + sizeof(Date) 
+constexpr int32_t position_of_to_date_in_file = 21; // sizeof(uint8_t) + 2 * sizeof(uint32_t) + sizeof(Date) 
 
 class Accommodation
 {
 private:
 	uint32_t room_number;
+	uint32_t number_of_guests;
 	Time_Period period;
 	String guest_name;
 	String note;
 public:
 	Accommodation() = default;
-	Accommodation(uint32_t, const Time_Period&, String, String);
+	Accommodation(uint32_t, uint32_t, const Time_Period&, String, String);
 	uint32_t get_room_number() const;
+	uint32_t get_number_of_guests() const;
 	const Time_Period& get_period() const;
 	const Date& get_from() const;
 	const Date& get_to() const;
