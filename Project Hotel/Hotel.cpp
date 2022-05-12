a878cfd06e8cc51ab9566b7e512246ada942f9af
Daniel Tropolinov <57357538+dani-tro@users.noreply.github.com>
2022-05-12 17:25:45 +0300

    Added definition of class Hotel

---
+++ Project Hotel/Hotel.cpp
@@ -0,0 +1,282 @@
+#include "Hotel.h"
+#include "String.h"
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+void Hotel::open_the_file_with_loaded_rooms()
+{
+	String name;
+	do
+	{
+		std::cout << "Enter the path to the file with the loaded rooms: ";
+		getline(std::cin, name, '\n');
+		if(name.get_data() != nullptr)file.open((const char*)name, std::ios::in | std::ios::out | std::ios::binary);
+		if (file.is_open() == false)std::cout << "There was an error loading the file! Try again!\n";
+		else std::cout << "File loaded successfully!\n";
+	} 
+	while (file.is_open() == false);
+}
+
+void Hotel::read_the_available_rooms_from_file()
+{
+	Room buff;
+	buff.read_room_from_file(file, file.tellg());
+	while (buff.get_room_number() != 0)
+	{
+		rooms.push_back(buff);
+		buff.read_room_from_file(file, file.tellg());
+	}
+	std::sort(rooms.begin(), rooms.end());
+	std::cout << "There are " << rooms.get_size() << " rooms at the hotel.\n";
+	
+}
+
+
+void Hotel::read_the_loaded_events_in_the_file()
+{
+	uint8_t type{0};
+	int32_t file_position;
+	uint32_t room_number{0};
+	Accommodation accommodation;
+	Closure closure;
+	file_position = file.tellg();
+	file.read(reinterpret_cast<char*>(&type), sizeof(uint8_t));
+	while (!file.eof())
+	{
+		if (type == 1)
+		{
+			accommodation.read_accommodation_from_file(file, file.tellg());
+			int32_t index = find_the_index_of_room_with_given_number(accommodation.get_room_number());
+			rooms[index].add_accommodation_index_to_the_list(file_position);
+		}
+		else if (type == 2)
+		{
+			closure.read_closure_from_file(file, file.tellg());
+			int32_t index = find_the_index_of_room_with_given_number(closure.get_room_number());
+			rooms[index].add_closure_index_to_the_list(file_position);
+		}
+		file_position = file.tellg();
+		file.read(reinterpret_cast<char*>(&type), sizeof(uint8_t));
+	}
+	file.clear();
+	return;
+}
+
+void Hotel::run_hotel()
+{
+	print_available_commands();
+	String command;
+	read_a_command(command);
+	while (command != "exit the app")
+	{
+		process_the_command(command);
+		read_a_command(command);
+	}
+	return;
+}
+
+int32_t Hotel::find_the_index_of_room_with_given_number(uint32_t room_number) const
+{
+	for (int i = 0; i < rooms.get_size(); i++)if (rooms[i].get_room_number() == room_number) return i;
+	return -1;
+}
+
+void Hotel::process_the_command(String& command)
+{
+	if (command == "register a guest")
+	{
+		register_a_guest();
+	}
+	else if (command == "print the free rooms")
+	{
+		print_the_free_rooms();
+	}
+	else if (command == "free a room")
+	{
+		free_a_room();
+	}
+	else if (command == "print a report")
+	{
+		print_a_report();
+	}
+	else if (command == "find a room")
+	{
+		find_a_room();
+	}
+	else if (command == "close a room")
+	{
+		close_a_room();
+	}
+	else
+	{
+		print_error_message();
+	}
+}
+
+void Hotel::register_a_guest()
+{
+	Accommodation accommodation;
+	std::cin >> accommodation;
+	if (is_valid(accommodation))
+	{
+		int index = find_the_index_of_room_with_given_number(accommodation.get_room_number());
+		file.seekp(0, file.std::ios::end);
+		rooms[index].add_accommodation_index_to_the_list(file.tellp());
+		accommodation.save(file);
+	}
+	else
+	{
+		std::cout << "Problem\n";
+	}
+}
+
+void Hotel::print_the_free_rooms()
+{
+	Date date;
+	std::cout << "Enter a date: ";
+	std::cin >> date;
+	Time_Period period(date, date);
+	Vector<uint32_t> free_rooms;
+	for (int i = 0; i < rooms.get_size(); i++)
+	{
+		if (rooms[i].is_available_during(period, file) == Condition::Available)free_rooms.push_back(rooms[i].get_room_number());
+	}
+	std::cout << "There are " << free_rooms.get_size() << " free rooms on " << date;
+	if (free_rooms.get_size() > 0)std::cout << ":";
+	std::cout << "\n";
+	for (int i = 0; i < free_rooms.get_size(); i++)std::cout << free_rooms[i] << std::endl;
+}
+
+void Hotel::free_a_room()
+{
+	std::cout << "Enter a room number: ";
+	uint32_t room_number, room_index;
+	Date date;
+	std::cin >> room_number;
+	std::cout << "Enter the date of departure: ";
+	std::cin >> date;
+	room_index = find_the_index_of_room_with_given_number(room_number);
+	int32_t index_of_accommodation_in_file = rooms[room_index].is_occupied_during(Time_Period(date, date), file);
+	if (index_of_accommodation_in_file == -1)
+	{
+		std::cout << "Problem\n";
+	}
+	else
+	{
+		Accommodation accommodation;
+		accommodation.change_the_departure_date_in_file(file, index_of_accommodation_in_file, date);
+		std::cout << "The room was vacated successfully!\n";
+	}
+}
+
+void Hotel::print_a_report()
+{
+	Time_Period period;
+	std::cin >> period;
+	String report_name = "report-";
+	report_name.strcat(period.get_from().to_String());
+	report_name.strcat(".txt");
+	std::fstream report;
+	report.open(report_name, std::ios::out);
+	for (int i = 0; i < rooms.get_size(); i++)
+	{
+		uint32_t number_of_days = rooms[i].number_of_days_being_occupied_in_period(period, file);
+		if (number_of_days > 0)
+		{
+			std::cout << "Room #" << rooms[i].get_room_number() << " was occupied " << number_of_days << " days.\n";
+			report << "Room #" << rooms[i].get_room_number() << " was occupied " << number_of_days << " days.\n";
+		}
+	}
+	std::cout << "End of the report\n";
+	return;
+}
+
+void Hotel::find_a_room()
+{
+	std::cout << "Enter the number of guests: ";
+	uint32_t number_of_guests;
+	std::cin >> number_of_guests;
+	Time_Period period;
+	std::cin >> period;
+	uint32_t room_number = get_the_room_with_the_fewest_beds(number_of_guests, period);
+	if (room_number == -1)std::cout << "There is no appropriate room!\n";
+	else std::cout << "The guest can be accommodated in room #" << room_number << std::endl;
+	return;
+}
+
+void Hotel::close_a_room()
+{
+	Closure closure;
+	std::cin >> closure;
+	if (is_valid(closure))
+	{
+		int index = find_the_index_of_room_with_given_number(closure.get_room_number());
+		file.seekp(0, file.std::ios::end);
+		rooms[index].add_closure_index_to_the_list(file.tellp());
+		closure.save(file);
+	}
+	else
+	{
+		std::cout << "Problem\n";
+	}
+}
+
+void Hotel::print_error_message()
+{
+}
+
+int32_t Hotel::get_the_room_with_the_fewest_beds(uint32_t number_of_guests, Time_Period& period)
+{
+	uint32_t i = 0;
+	while(i < rooms.get_size() && (rooms[i].get_number_of_beds() < number_of_guests ||
+		rooms[i].is_available_during(period, file) != Condition::Available))
+	{
+		i++;
+	}
+	if (i == rooms.get_size())return -1;
+	return rooms[i].get_room_number();
+}
+
+bool Hotel::is_valid(const Accommodation& accommodation)
+{
+	
+	int32_t index = find_the_index_of_room_with_given_number(accommodation.get_room_number());
+	if (index < 0)return false; 
+	if (accommodation.get_period().is_valid() == false)return false;
+	
+	return (rooms[index].is_available_during(accommodation.get_period(), file) == Condition::Available);
+}
+
+bool Hotel::is_valid(const Closure& closure)
+{
+	int32_t index = find_the_index_of_room_with_given_number(closure.get_room_number());
+	if (index < 0)return false;
+	if (closure.get_period().is_valid() == false)return false;
+
+	return (rooms[index].is_available_during(closure.get_period(), file) == Condition::Available);
+}
+
+
+void print_available_commands()
+{
+	std::cout << "------------------------------------------------------------------------------------------------------------------------\n";
+	std::cout << "TO PERFORM OPERATIONS, ENTER SOME OF THE FOLLOWING COMMANDS: (CASE-INSENSITIVE)\n";
+	std::cout << "REGISTER A GUEST\n";
+	std::cout << "PRINT THE FREE ROOMS\n";
+	std::cout << "FREE A ROOM\n";
+	std::cout << "PRINT A REPORT\n";
+	std::cout << "FIND A ROOM\n";
+	std::cout << "CLOSE A ROOM\n";
+	std::cout << "EXIT THE APP\n";
+	std::cout << "------------------------------------------------------------------------------------------------------------------------\n";
+}
+
+void read_a_command(String& command)
+{
+	getline(std::cin, command, '\n');
+	command.tolower();
+	return;
+}
+
